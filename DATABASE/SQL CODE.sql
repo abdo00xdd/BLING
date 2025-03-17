61f99ab552151d3e64daf766400985f09e6ef990
mxrtixn <hamzaelma96@gmail.com>
2025-03-17 21:23:48 +0000

    bling_db Tables

--- DATABASE/SQL CODE.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE bling_db;
-USE bling_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    hashed_password VARCHAR(255) NOT NULL
-);
